a3e52c8895ba39f5f257b318f505a6ad243311bf
tauren-81@mail.ru <tauren-81@mail.ru>
2025-04-04 04:01:56 +0400

    Шляпа какая-то, плагин check-file упрямо плюет на все наименования, хоть как назови
    Добавил папки с будущими сборщиками

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
 import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
+import checkFile from 'eslint-plugin-check-file'
 
 export default defineConfig([
+    globalIgnores(['.config/', 'node_modules/*', '**/.git', '**/.vscode']),
     {
         files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
         rules: {
-            'prefer-const': 'warn',
             'no-constant-binary-expression': 'error',
             'no-var': 'error',
             indent: 'error',
@@ -29,6 +30,26 @@ export default defineConfig([
         files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
         plugins: { js },
         extends: ['js/recommended'],
+    },
+    {
+        files: ['src/**/'],
+        plugins: { checkFile },
+        rules: {
+            'check-file/filename-naming-convention': [
+                'error',
+                {
+                    '**/*.{jsx,tsx}': 'PASCAL_CASE',
+                    '**/*.{js,ts}': 'CAMEL_CASE',
+                },
+            ],
+            'check-file/folder-naming-convention': [
+                'error',
+                {
+                    'src/**/': 'CAMEL_CASE',
+                    'src/!(components)/**/!(__tests__)/': 'CAMEL_CASE',
+                },
+            ],
+        },
     },
     tseslint.configs.recommended,
     pluginReact.configs.flat.recommended,
