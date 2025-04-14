0673e94d849f00f031558f2b247f74e071b6a8fc
tauren-81@mail.ru <tauren-81@mail.ru>
2025-04-14 07:23:18 +0400

    пока не понятно почему не видит

--- NextjsPanda/app/page.tsx
+++ NextjsPanda/app/page.tsx
@@ -1,19 +1,17 @@
 import Button from '@components/button/Button';
-//import Card from '@components/card/Card';
+import Card from '@components/card/Card';
 import React from 'react';
 
 const Page = () => {
   return (
     <div>
       <h1>ТУТА Я</h1>
-      {/*
       <h1>
         <Card />
       </h1>
       <h3>
         <Button />
       </h3>
-      */}
     </div>
   );
 };
