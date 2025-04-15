ad7344df97559ed3ea4ebf7eb577e7b37b7264ca
tauren-81@mail.ru <tauren-81@mail.ru>
2025-04-15 04:46:11 +0400

    Card на Tailwind. Button - без Emotion и даже не кнопка

--- ViteEmotion/src/app/index.tsx
+++ ViteEmotion/src/app/index.tsx
@@ -1,32 +1,10 @@
 import React from 'react';
-import CustomTab from '@components/CustomTab/CustomTab';
-import CustomButton from '@components/CustomButton/CustomButton';
-import CustomRating from '@components/CustomRating/CustomRating';
-import CustomCalendar from '@components/CustomCalendar/CustomCalendar';
-import CustomModal from '@components/CustomModal/CustomModal';
+import Card from '@components/card/Card';
 
 function App() {
   return (
     <div>
-      {/* стили Chakra-ui перекрывают стили tailwind поэтому тут закомментировано
-      нужно показывать или этот компонент или MyModal*/}
-
-      <h3>
-        <CustomTab />
-      </h3>
-      <h3>
-        <CustomButton />
-      </h3>
-      <h3>
-        <CustomRating />
-      </h3>
-      <h3>
-        <CustomCalendar />
-      </h3>
-
-      <h3>
-        <CustomModal />
-      </h3>
+      <Card />
     </div>
   );
 }
