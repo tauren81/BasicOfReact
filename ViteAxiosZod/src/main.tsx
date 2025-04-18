07dd11ff1b7bb125fb8210e596ad136464ea1689
tauren-81@mail.ru <tauren-81@mail.ru>
2025-04-18 07:32:43 +0400

    Отсутсвует роутинг и не подключена валидация

--- ViteAxiosZod/src/main.tsx
+++ ViteAxiosZod/src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client';
 
 import App from './app';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
+const queryClient = new QueryClient();
+
 const root = document.getElementById('root');
 if (!root) throw new Error('No root element found');
 
 createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
--- ViteAxiosZod/src/types/api-client.ts
+++ ViteAxiosZod/src/types/api-client.ts
@@ -1,12 +1,8 @@
 import axios from 'axios';
 
-const AxiosInstance = () => {
-  return axios.create({
-    baseURL: 'http://localhost:8080/api',
-    headers: {
-      'Content-type': 'application/json',
-    },
-  });
-};
-
-export default AxiosInstance;
+export default axios.create({
+  baseURL: 'http://localhost:8080/api',
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
